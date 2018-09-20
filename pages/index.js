8293ff32526ed7d2df1b1c05014cf8c9da1cba84
deusexmachina892 <deusexmachina892@gmail.com>
2018-09-20 10:00:04 -0400

    Application Server initiated

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Lottery extends Component{
+    render(){
+        return(
+            <h1>Lottery Dapp</h1>
+        );
+    }
+}
+
+export default Lottery;
