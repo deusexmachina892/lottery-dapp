df6152cdd88af8b00ab703460b7573e89b4f14d9
deusexmachina892 <deusexmachina892@gmail.com>
2018-09-21 10:35:44 -0400

    Contract Deployed

--- deploy.js
+++ deploy.js
@@ -14,6 +14,16 @@ const web3 = new Web3(provider);
 const deploy = async ()=>{
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
+    console.log('Contract is deployed by the manager at address', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+                            .deploy({data: '0x' + bytecode})
+                            .send({gas: '200000', from: accounts[0]});
+
+    console.log('Contract deployed to address', result.options.address);
+
+    //Contract deployed at: 0xfA716848a1977cCD275dD6850699aEEc1A8E2Ca2
+
 }
 
 deploy();
